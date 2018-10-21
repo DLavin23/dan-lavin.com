40b993d9d0c0980c2f44e6e2f6724b3cd2efc2a9
Dan Lavin <daniel.e.lavin@gmail.com>
2018-10-21 11:32:14 -0500

    WTF is wrong with this build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import Box from '../components/Box'
 
 // move to seperate file
