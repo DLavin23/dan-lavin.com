62ff7dccb72b4acf978ed2f8d320f2d42e4c03a6
Dan Lavin <daniel.e.lavin@gmail.com>
2018-11-13 21:56:02 -0600

    adds work page

---
+++ src/pages/work.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Hero from '../components/Hero'
+import Text from '../components/Text'
+import Section from '../components/Section'
+import SectionHeader from '../components/SectionHeader'
+
+const Work = () => (
+  <Layout>
+    <Hero>
+      <Text>
+        Work History
+      </Text>
+    </Hero>
+    <Section>
+      <SectionHeader title="Work" />
+      <ul>
+
+      </ul>
+    </Section>
+  </Layout>
+)
+
+export default Work
