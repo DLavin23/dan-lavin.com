1945161ea827f8bef85658fa3b22494aa8020d7b
Dan Lavin <daniel.e.lavin@gmail.com>
2021-12-29 11:59:54 -0600

    images dir

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
   /* --------------------------------------------------------------------------
   11ty settings
   -------------------------------------------------------------------------- */
-  eleventyConfig.addPassthroughCopy({'source/assets/images': 'assets'});
+  eleventyConfig.addPassthroughCopy({'source/assets/images': 'assets/images'});
   eleventyConfig.addPassthroughCopy({'source/assets/css': 'assets'});
   eleventyConfig.addPassthroughCopy({'source/assets/js': 'assets'});
   eleventyConfig.addPassthroughCopy({'node_modules/modern-normalize/*.css': 'assets'});
